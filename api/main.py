f1f65f418716def9d57035a2f73ba9954e7f8c98
yyk722 <37979276+yyk722@users.noreply.github.com>
2023-05-05 07:41:37 +0800

    added api

--- api/main.py
+++ api/main.py
@@ -26,6 +26,7 @@ def predict_trading_volume(input_data: InputData):
     tema = input_data.tema
     
     frame = h2o.H2OFrame({"Symbol":symbol, "etf":etf, "rsi":rsi, "tema":tema, "vol_moving_avg":vol_moving_avg, "adj_close_rolling_med":adj_close_rolling_med})
-    
+    result = saved_model.predict(frame)
+    print(result.as_data_frame()[1][0])
     # Return the predicted trading volume as a response
-    return {"trading_volume": saved_model.predict(frame)}
+    return {"trading_volume": result.as_data_frame()[1][0]}
